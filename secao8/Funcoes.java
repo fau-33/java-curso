8ad856bc65be256a9761eb5a471e83ae03d7ab44
Flávio Félix <fau_felix30@hotmail.com>
2025-01-17 19:38:29 -0300

    Começando os estudos de função em java

---
+++ secao8/Funcoes.java
@@ -0,0 +1,13 @@
+package secao8;
+
+public class Funcoes {
+    public static void main(String[] args) {
+        
+        // 1 - Criando a primeira função
+        saudacao();
+    }
+
+    public static void saudacao() {
+        System.out.println("Ola, seja bem vindo!");
+    }
+}
