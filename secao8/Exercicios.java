57ec55386afb282ea54550b4ba350ab19c876feb
Flávio Félix <fau_felix30@hotmail.com>
2025-01-31 20:19:45 -0300

    Resolvido os 6 exercicios sobre função

--- secao8/Exercicios.java
+++ secao8/Exercicios.java
@@ -51,10 +51,91 @@ public class Exercicios {
     double celsius = 50.0;
     double fahrenheit = converterCelsiusParaFahrenheit(celsius);
     System.out.println("A temperatura de  " + celsius + " C é equivalente a " + fahrenheit + " F");
+
+    // Exercicio 2: Função para calcular fatorial
+    int n = 20;
+    int fatorial = calcularFatorial(n);
+    System.out.println("O fatorial de " + n + " eh " + fatorial);
+
+    // Exercicio 3: Função com if-else para verificar paridade
+    int numero = 56;
+    String paridade = verificarParidade(numero);
+    System.out.println("O numero " + numero + " eh " + paridade);
+
+    // Exercicio 4: Função com switch para classificar notas
+    int nota = 5;
+    String classificacao = classificarNota(nota);
+    System.out.println("A nota " + nota + " eh " + classificacao);
+
+    // Exercicio 5: Função com System.exit para verificar idade
+    int idade = 20;
+    System.out.println(idade);
+    verificarIdade(idade);
+
+    // Exercicio 6: Função que recebe um array e retorna o maior número
+    int[] numeros = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+    int maiorNumero = encontrarMaiorNumero(numeros);
+    System.out.println("O maior número do array eh: " + maiorNumero);
+
   }
 
   // Função
   public static double converterCelsiusParaFahrenheit(double celsius) {
     return (celsius * 9 / 5) + 32;
+  }
+
+  public static int calcularFatorial(int n) {
+    int fatorial = 1;
+    for (int i = 1; i <= n; i++) {
+      fatorial *= i;
+    }
+    return fatorial;
+  }
+
+  public static String verificarParidade(int numero) {
+    if (numero % 2 == 0) {
+      return "Par";
+    } else {
+      return "Impar";
+    }
+  }
+
+  public static String classificarNota(int nota) {
+    if (nota < 0 || nota > 10) {
+      return "Nota inválida";
+    }
+
+    switch (nota) {
+      case 10:
+      case 9:
+        return "A";
+      case 8:
+        return "B";
+      case 7:
+        return "C";
+      case 6:
+        return "D";
+      default:
+        return "F";
+    }
+  }
+
+  public static void verificarIdade(int idade) {
+    if (idade < 18) {
+      System.out.println("Acesso negado");
+      System.exit(0);
+    } else {
+      System.out.println("Acesso permitido");
+    }
+  }
+
+  public static int encontrarMaiorNumero(int[] numeros) {
+    int maior = numeros[0];
+    for (int i = 1; i < numeros.length; i++) {
+      if (numeros[i] > maior) {
+        maior = numeros[i];
+      }
+    }
+    return maior;
   }
 }
