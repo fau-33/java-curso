619d31851c8df7a1128cec2f0a945113b0fad7a5
Flávio Félix <fau_felix30@hotmail.com>
2025-03-24 17:22:34 -0300

    Feito as aulas sobre arrays

--- secao14/Listas.java
+++ secao14/Listas.java
@@ -1,5 +1,9 @@
 package secao14;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Listas {
     public static void main(String[] args) {
         
@@ -51,6 +55,131 @@ public class Listas {
             
         }
         System.out.println("O maior elemento é: " + maior);
+
+        // 3 - for each
+        for(int numero: numeros){
+            System.out.println("O numero da vez é: " + numero);
+        }
+
+        // Concatenar elementos em um array
+        String[] palavras = {"Java", "é", "Bom"};
+
+        String frase = "";
+
+        for (String palavra : palavras) {
+            frase += palavra + " ";
+        }
+        System.out.println(frase);
+
+        // Verificar se valor está presente em um array
+        char[] letras = {'a', 'b', 'c', 'd', 'e'};
+
+        char letraProcurada  = 'c';
+
+        for(char letra: letras){
+            if(letra == letraProcurada){
+                System.out.println("A letra " + letraProcurada + " foi encontrada");
+                break;
+            }
+        }
+        // 5 - Loop com condicionais if
+        // filtar e somar numeros pares
+        int somaPares = 0;
+
+        for(int numero: numeros){
+            if(numero % 2 == 0){
+                somaPares += numero;
+            }
+        }
+        System.out.println("A soma dos numeros pares é: " + somaPares);
+
+        // Exibir valores maiores que um determinado valor
+        int[] nums = {1, 200, 300, 400, 200, 6, 700, 8};
+
+        int limite = 10;
+
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] > limite){
+                System.out.println("O numero " + nums[i] + " eh maior que " + limite);
+            }
+        }
+
+        String[] linguagens = {"Java", "Python", "C++", "C#"};
+
+        String linguagemAlvo = "Java";
+
+        for(String linguagem: linguagens){
+            if(linguagem.equals(linguagemAlvo)){
+                System.out.println("A linguagem " + linguagem + " foi encontrada parando o loop");
+                break;
+            }else {
+                System.out.println("A linguagem " + linguagem + " nao foi encontrada");
+            }
+        }
+
+        // 6 - Atualizar valores em um array
+        for(int i = 0; i < numeros.length; i++){
+            numeros[i] *= 2;
+        }
+        System.out.println(numeros[1]);
+
+        for(int numero: numeros){
+            System.out.println("O numero dobrado é: " + numero);
+        }
+
+        numeros[5] = 1;
+        System.out.println(numeros[5]);
+
+        for(int i = 0; i < frutas.length; i++){
+            if(frutas[i].equals("Banana")){
+                frutas[i] = "Abacaxi";
+            }
+        }
+
+        System.out.println(frutas[0]);
+
+        // 7 - Metodos to String
+        //Classes java, que vai ter método em arrays são métodos utilitários
+        String dadosNumericos = Arrays.toString(numeros);
+
+        System.out.println(dadosNumericos);
+
+        String dadosFrutas = Arrays.toString(frutas);
+
+        System.out.println(dadosFrutas);
+
+        int[] teste = new int[3];
+
+        System.out.println(Arrays.toString(teste));
+
+        // 8 - Adicinando novos itens em um array
+        // ex1: Criano um novo array maior e copiando os elementos antigos
+        int[] novoArray = new int[numeros.length + 1];
+
+        for(int i = 0; i < numeros.length; i++){
+            novoArray[i] = numeros[i];
+        }
+        System.out.println(Arrays.toString(novoArray));
+
+        novoArray[novoArray.length - 1] = 100;
+
+        System.out.println(Arrays.toString(novoArray));
+
+        // ex2: Utlizando arraycop para copiar os elementos
+        String[] novoFrutas = new String[frutas.length + 1];
+        System.arraycopy(frutas, 0, novoFrutas, 0, frutas.length);
+
+        novoFrutas[novoFrutas.length - 1] = "Laranja";
+
+        System.out.println(Arrays.toString(novoFrutas));
+
+        // ex3: Usando arraylist
+        ArrayList<String> listaFrutas = new ArrayList<>(Arrays.asList("Maçã", "Banana", "Abacaxi"));
+
+        System.out.println(listaFrutas); 
+        listaFrutas.add("Manga");
+
+        System.out.println(listaFrutas);
     }
 
 }
